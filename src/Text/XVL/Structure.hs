3aed4c99d97b78f1871b9fbe0217d3a44a2bafb7
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2011-11-19 14:10:57 +0100

    Setting up the data structures

---
+++ src/Text/XVL/Structure.hs
@@ -0,0 +1,17 @@
+{- Contains the types describing the structure of XVL document. -}
+module Text.XVL.Structure where
+
+
+type XVLDocument = [XVLItem]
+
+data XVLItem = XVLSection {
+                   xvlSectionName :: String,
+                   xvlSectionItems :: [XVLItem]
+               }
+             | XVLKeyValue {
+                   xvlKey :: String,
+                   xvlValue :: Maybe XVLValue
+               }
+               
+data XVLValue = XVLText String
+              | XVLArray [XVLValue]
